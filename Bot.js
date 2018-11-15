6d97b334ff4dd27bb8a3ea91281b6db212864a53
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2018-11-15 23:22:21 +0100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -12,7 +12,7 @@ client.on("ready", () => {
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find(channel => channel.name === "hangar");
   if (!channel) return;
-  channel.send(`Bzzzzzzz Bi-en-venido, forma de vida no identificada de nombre ${member}, por favor, facilitanos tus datos principales para que podamos tramitar tu petición de acceso a un @Ingeniero. \n\n Les recordamos que en el interior de la base deben seguirse las normas de comportamiento de la comunidad (Hablar roleando), cualquier tipo de infracción de estas normas podría conllevar la expulsión permanente de la nave. Tengan una agradable estancia. Bzzzzzz *Sonidos de estática*`);
+  channel.send(`Bzzzzzzz Bi-en-venido, forma de vida no identificada de nombre ${member}, por favor, facilitanos tus datos principales para que podamos tramitar tu petición de acceso a un <@&511984031517114388>. \n\n Les recordamos que en el interior de la base deben seguirse las normas de comportamiento de la comunidad (Hablar roleando), cualquier tipo de infracción de estas normas podría conllevar la expulsión permanente de la nave. Tengan una agradable estancia. Bzzzzzz *Sonidos de estática*`);
 });
 
 client.login(process.env.BOT_TOKEN);
