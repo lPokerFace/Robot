9fc05e452d2884dcf2e1fec8927380d5a0005e39
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2018-11-15 21:25:38 +0100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+let args = message.content.split(' ').slice(1)
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
