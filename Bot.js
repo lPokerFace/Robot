a9b93b4d15acf1e7d183e6cfd7435bcb842ba516
lPokerFace <34305565+lPokerFace@users.noreply.github.com>
2018-11-15 21:39:49 +0100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -3,15 +3,14 @@ const client = new Discord.Client();
 
 client.on('message', message => {
   let args = message.content.split(' ').slice(1)
-  });
+});
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'bienvenida');
-  const copiar = member.guild.channels.find('name', args[1]);
+  const channel = member.guild.channels.find(channel => channel.name === "recepción");
   if (!channel) return;
   channel.send(`Bzzzzzzz Bi-en-venido, forma de vida no identificada de nombre ${member}, por favor, facilitanos tus datos principales para que podamos tramitar tu petición de acceso a un @Ingeniero. \n\n Les recordamos que en el interior de la base deben seguirse las normas de comportamiento de la comunidad (Hablar roleando), cualquier tipo de infracción de estas normas podría conllevar la expulsión permanente de la nave. Tengan una agradable estancia. Bzzzzzz *Sonidos de estática*`);
 });
